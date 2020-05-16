9ae064cce61562b8d56c0b72bb7a58ea9ba6e9b5
nick wuerz <nawuerz@gmail.com>
2020-05-15 22:24:10 -0500

    server

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const {from, subject, text} = req.body
     });
 });
 
-app.use(function(req, res) {
+router.use(function(req, res) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 
